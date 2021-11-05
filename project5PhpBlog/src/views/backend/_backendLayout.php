321e6a152a669bcadfe3c7b0264d0bb5133861fc
Jenny Chen <pelgrims.chenchingyi@gmail.com>
2021-11-05 13:20:30 +0100

    update backend views

--- project5PhpBlog/src/views/backend/_backendLayout.php
+++ project5PhpBlog/src/views/backend/_backendLayout.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?=htmlspecialchars($title)?></title>
+    <title><?=filter_var($title, FILTER_SANITIZE_STRING)?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
